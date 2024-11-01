bd55b39c42f716e4c2f5138b9a0f5ac52885dd07
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-01 21:35:39 +0200

    Create Navbar.vue

---
+++ tetranoma/src/components/navbar/Navbar.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Mobile from './Mobile.vue'
+import Tablet from './Tablet.vue'
+import Web from './Web.vue'
+</script>
+
+<template>
+    <div class="hidden w-full h-full sm:flex-col sm:flex">
+        <Mobile />
+    </div>
+    <div class="hidden w-full h-full md:flex">
+        <Tablet />
+    </div>
+    <div class="hidden w-full h-full items-center justify-center lg:flex xl:flex 2xl:flex">
+        <Web />
+    </div>
+</template>
