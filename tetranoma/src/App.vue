953592d332bb336ffbdca238fe584b2aaf21e546
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-01 21:35:01 +0200

    Create App.vue

---
+++ tetranoma/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import Navbar from '@/components/navbar/Navbar.vue'
+import Footer from '@/components/footer/Footer.vue'
+</script>
+
+<template>
+    <div class="w-full h-full flex flex-col items-center bg-[#FAFAFA]">
+        <Navbar />
+        <RouterView class="w-full h-full flex justify-center" />
+        <Footer />
+    </div>
+</template>
