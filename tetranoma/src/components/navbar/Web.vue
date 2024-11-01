ba04ef29cec3b648b331aacc11519ad3f8334f5a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-01 21:35:44 +0200

    Create Web.vue

---
+++ tetranoma/src/components/navbar/Web.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+import logo from '@/assets/logo.jpg'
+</script>
+
+<template>
+    <div class="flex items-center justify-between  max-w-[1336px] w-full h-full">
+        <RouterLink to="/" class="">
+            <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
+        </RouterLink>
+        <div class="flex gap-[50px] lg:text-lg xl:text-lg 2xl:text-xl">
+            <RouterLink to="/about" class="tracking-wide">About
+            </RouterLink>
+            <RouterLink to="/contact" class="tracking-wide">Contact
+            </RouterLink>
+            <RouterLink to="/list" class="tracking-wide">List
+            </RouterLink>
+            <RouterLink to="/add" class="tracking-wide">Add
+            </RouterLink>
+        </div>
+    </div>
+</template>
