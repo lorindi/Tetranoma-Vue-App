3284ff2572161e5ed144e0e4d4e43b70746f28a6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 12:37:17 +0200

    Create Search.vue

---
+++ tetranoma/src/components/search/Search.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="flex border border-spacing-2">
+        <input type="text" placeholder="Search ...">
+        <button>Search</button>
+
+    </div>
+</template>
