1d3cfba09792f04686b8a586d5b2cb9f4f16588b
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 22:18:18 +0200

    Update Footer.vue

--- tetranoma/src/components/footer/Footer.vue
+++ tetranoma/src/components/footer/Footer.vue
@@ -1,7 +1,43 @@
-<script setup></script>
+<script setup>
+import NavigationLinks from '../NavigationLinks.vue'
 
+</script>
 <template>
-    <div class="">
-        <h1>footer</h1>
-    </div>
+    <footer class="flex w-full max-w-[1336px] items-start justify-around p-[30px]">
+
+        <div class="flex flex-col gap-[30px]">
+            <div class="">
+                <h2 class="">Tetranoma</h2>
+                <p class="text-gray-600 mt-2 text-center md:text-left">
+                    Your one-stop solution for 3D printed figures.
+                </p>
+            </div>
+
+            <div class="">
+                <div class="">
+                    <p>Privacy Policy | Terms of Service | FAQ</p>
+                </div>
+                <div class="">
+                    <p>&copy; 2024 Tetranoma. All rights reserved.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-[10px]">
+            <NavigationLinks />
+        </div>
+
+        <div class="flex gap-[20px]">
+            <a href="#" class="text-gray-700 hover:text-[#42BB8B]">
+                <i class="pi pi-facebook"></i>
+            </a>
+            <a href="#" class="text-gray-700 hover:text-[#42BB8B]">
+                <i class="pi pi-twitter"></i>
+            </a>
+            <a href="#" class="text-gray-700 hover:text-[#42BB8B]">
+                <i class="pi pi-instagram"></i> 
+            </a>
+        </div>
+
+    </footer>
 </template>
