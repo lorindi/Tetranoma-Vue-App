5c3a54e336bb115491cd37d27c325153fa1abe79
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 21:46:07 +0200

    Update Tablet.vue

--- tetranoma/src/components/navbar/Tablet.vue
+++ tetranoma/src/components/navbar/Tablet.vue
@@ -3,11 +3,9 @@ import { ref } from 'vue'
 import { RouterLink, useRoute } from 'vue-router'
 import Search from '../search/Search.vue'
 import logo from '@/assets/logo.jpg'
+import NavigationLinks from './NavigationLinks.vue'
+import ToggleButton from './ToggleButton.vue';
 
-const isActiveLink = (routePath) => {
-    const route = useRoute()
-    return route.path === routePath
-}
 
 const isOpen = ref(false)
 
@@ -22,39 +20,14 @@ const toggleMenu = () => {
             <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
         </RouterLink>
 
-        <button @click="toggleMenu" class="flex flex-col gap-[4.5px] cursor-pointer z-20">
-            <div
-                :class="['w-6 h-1 bg-[#42BB8B] rounded-sm origin-left ease-in-out duration-500', isOpen ? 'rotate-45' : '']">
-            </div>
+        <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
 
-            <div :class="['w-6 h-1 bg-[#42BB8B] rounded-sm ease-in-out duration-500', isOpen ? 'opacity-0' : '']">
-            </div>
-
-            <div
-                :class="['w-6 h-1 bg-[#42BB8B] rounded-sm origin-left ease-in-out duration-500', isOpen ? '-rotate-45' : '']">
-            </div>
-        </button>
         <nav v-motion :initial="{ opacity: 0, translateY: -20 }" :enter="{ opacity: 1, translateY: 0 }"
             :leave="{ opacity: 0, translateY: -20 }"
             :transition="{ enter: { duration: 0.5 }, leave: { duration: 1.0 } }" v-if="isOpen"
             class="absolute flex flex-col w-[100%] items-center justify-center top-[125px] left-0 bg bg-[#fafafa] h-[250px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
             <div class="flex gap-10">
-                <RouterLink to="/about"
-                    :class="[isActiveLink('/about') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
-                    About
-                </RouterLink>
-                <RouterLink to="/contact"
-                    :class="[isActiveLink('/contact') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
-                    Contact
-                </RouterLink>
-                <RouterLink to="/list"
-                    :class="[isActiveLink('/list') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
-                    List
-                </RouterLink>
-                <RouterLink to="/add"
-                    :class="[isActiveLink('/add') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
-                    Add
-                </RouterLink>
+                <NavigationLinks />
             </div>
             <Search size="medium" />
         </nav>
