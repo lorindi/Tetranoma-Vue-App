ac4782708d4205cd39a4c4b2461d3a5805750268
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 12:37:14 +0200

    Update Web.vue

--- tetranoma/src/components/navbar/Web.vue
+++ tetranoma/src/components/navbar/Web.vue
@@ -1,21 +1,31 @@
 <script setup>
-import { RouterLink } from 'vue-router'
+import { RouterLink, useRoute } from 'vue-router'
 import logo from '@/assets/logo.jpg'
+
+const isActiveLink = (routePath) => {
+    const route = useRoute()
+    return route.path === routePath
+}
 </script>
 
 <template>
-    <div class="flex items-center justify-between  max-w-[1336px] w-full h-full">
+    <div class="flex items-center justify-between  max-w-[1336px] w-full h-full px-[30px]">
         <RouterLink to="/" class="">
             <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
         </RouterLink>
-        <div class="flex gap-[50px] lg:text-lg xl:text-lg 2xl:text-xl">
-            <RouterLink to="/about" class="tracking-wide">About
+        <div class="flex gap-[50px] text-lg">
+            <RouterLink to="/about"
+                :class="[isActiveLink('/about') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide']">About
             </RouterLink>
-            <RouterLink to="/contact" class="tracking-wide">Contact
+            <RouterLink to="/contact"
+                :class="[isActiveLink('/contact') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide']">
+                Contact
             </RouterLink>
-            <RouterLink to="/list" class="tracking-wide">List
+            <RouterLink to="/list"
+                :class="[isActiveLink('/list') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide']">List
             </RouterLink>
-            <RouterLink to="/add" class="tracking-wide">Add
+            <RouterLink to="/add"
+                :class="[isActiveLink('/add') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide']">Add
             </RouterLink>
         </div>
     </div>
