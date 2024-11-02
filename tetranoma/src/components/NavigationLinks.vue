074f53218b968781bdbf1b606cfa462260839443
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 22:18:28 +0200

    Create NavigationLinks.vue

---
+++ tetranoma/src/components/NavigationLinks.vue
@@ -0,0 +1,28 @@
+<!-- NavigationLinks.vue -->
+<script setup>
+import { RouterLink, useRoute } from 'vue-router'
+
+const links = [
+  { path: '/about', label: 'About' },
+  { path: '/contact', label: 'Contact' },
+  { path: '/list', label: 'List' },
+  { path: '/add', label: 'Add' },
+]
+
+const isActiveLink = (routePath) => {
+  const route = useRoute()
+  return route.path === routePath
+}
+</script>
+
+<template>
+  <RouterLink to="/"
+    :class="[isActiveLink('/') ? 'text-[#42BB8B]' : '', 'xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden  hover:text-[#117277] tracking-wide text-lg']">
+    <i :class="['pi pi-home text-[#117277]', iconClasses]"></i>
+
+  </RouterLink>
+  <RouterLink v-for="link in links" :key="link.path" :to="link.path"
+    :class="[isActiveLink(link.path) ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide text-lg']">
+    {{ link.label }}
+  </RouterLink>
+</template>
