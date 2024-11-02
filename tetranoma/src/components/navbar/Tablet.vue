e2a207cbf41e952f8e0b3764d7987f87e3819a1d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-02 19:17:12 +0200

    Update Tablet.vue

--- tetranoma/src/components/navbar/Tablet.vue
+++ tetranoma/src/components/navbar/Tablet.vue
@@ -34,26 +34,33 @@ const toggleMenu = () => {
                 :class="['w-6 h-1 bg-[#42BB8B] rounded-sm origin-left ease-in-out duration-500', isOpen ? '-rotate-45' : '']">
             </div>
         </button>
-        <nav v-if="isOpen"
-            class="absolute flex flex-col w-[100%] items-center justify-center top-[125px] left-0 bg bg-[#fafafa] h-[250px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
-            <!-- <div class="absolute bg-cover bg-center bg-custom-image h-full w-full z-[-5] opacity-30 shadow-inner shadow-[#FAFAFA] rounded-br-lg rounded-bl-lg"></div> -->
+        <motion.nav v-if="isOpen" 
+            initial="{ opacity: 0, translateY: -20 }" 
+            animate="{ opacity: 1, translateY: 0 }"
+            exit="{ opacity: 0, translateY: -20 }" 
+            transition="{ duration: 15}"
+            class="absolute flex flex-col w-[100%] items-center justify-center top-[125px] left-0 bg bg-[#fafafa] h-[250px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400"
+            >
             <div class="flex gap-10">
                 <RouterLink to="/about"
-                    :class="[isActiveLink('/about') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">About
+                    :class="[isActiveLink('/about') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
+                    About
                 </RouterLink>
                 <RouterLink to="/contact"
                     :class="[isActiveLink('/contact') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
                     Contact
                 </RouterLink>
                 <RouterLink to="/list"
-                    :class="[isActiveLink('/list') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">List
+                    :class="[isActiveLink('/list') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
+                    List
                 </RouterLink>
                 <RouterLink to="/add"
-                    :class="[isActiveLink('/add') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">Add
+                    :class="[isActiveLink('/add') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
+                    Add
                 </RouterLink>
             </div>
-            <Search/>
-        </nav>
+            <Search />
+        </motion.nav>
     </div>
 
 </template>
