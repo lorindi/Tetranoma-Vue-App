d02d318f4a361fe91d1abe5d49885d2abcf33a97
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-03 17:28:21 +0200

    Update main.js

--- tetranoma/src/main.js
+++ tetranoma/src/main.js
@@ -5,9 +5,13 @@ import App from './App.vue'
 import router from './router'
 import { MotionPlugin } from '@vueuse/motion';
 import 'primeicons/primeicons.css';
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
 
 const app = createApp(App)
 
+
 app.use(router)
 app.use(MotionPlugin);
+app.use(Toast);
 app.mount('#app')
