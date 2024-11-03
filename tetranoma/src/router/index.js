04680d8e10a0e7351c2e0f13734382bdd753ff74
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-03 16:36:33 +0200

    Update index.js

--- tetranoma/src/router/index.js
+++ tetranoma/src/router/index.js
@@ -1,4 +1,8 @@
+import AboutView from '@/views/AboutView.vue'
+import AddFigureView from '@/views/AddFigureView.vue'
+import ContactView from '@/views/ContactView.vue'
 import CreateAccountView from '@/views/CreateAccountView.vue'
+import FiguresView from '@/views/FiguresView.vue'
 import HomeView from '@/views/HomeView.vue'
 import SignInView from '@/views/SignInView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -20,6 +24,26 @@ const router = createRouter({
             path: '/create-account',
             name: 'create-account',
             component: CreateAccountView,
+        },
+        {
+            path: '/figures',
+            name: 'figures',
+            component: FiguresView,
+        },
+        {
+            path: '/add-figure',
+            name: 'add-figure',
+            component: AddFigureView,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AboutView,
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: ContactView,
         },
     ],
 })
