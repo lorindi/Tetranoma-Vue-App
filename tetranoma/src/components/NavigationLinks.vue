e47b0ef334eb05688bf0f8a6bbb5d8f18e26cf4e
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-03 17:04:49 +0200

    Update NavigationLinks.vue

--- tetranoma/src/components/NavigationLinks.vue
+++ tetranoma/src/components/NavigationLinks.vue
@@ -19,8 +19,18 @@ const isActiveLink = (routePath) => {
   <RouterLink to="/"
     :class="[isActiveLink('/') ? 'text-[#42BB8B]' : '', 'xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden  hover:text-[#117277] tracking-wide text-lg']">
     <i :class="['pi pi-home text-[#117277]']"></i>
-
   </RouterLink>
+
+  <RouterLink to="/sign-in"
+    :class="[isActiveLink('/sign-in') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
+    Sign In
+  </RouterLink>
+  
+  <RouterLink to="/create-account"
+    :class="[isActiveLink('/create-account') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
+    Create Account
+  </RouterLink>
+
   <RouterLink v-for="link in links" :key="link.path" :to="link.path"
     :class="[isActiveLink(link.path) ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide text-lg']">
     {{ link.label }}
