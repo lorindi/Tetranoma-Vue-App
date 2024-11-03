b84a2cc7c22f82c5ed21438daf3db5a763e691cb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-03 14:22:51 +0200

    Update SignInView.vue

--- tetranoma/src/views/SignInView.vue
+++ tetranoma/src/views/SignInView.vue
@@ -1,12 +1,12 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 
 </script>
 
 <template>
-    <div class="absolute bg-custom-image w-full h-[calc(100vh-125px)] bg-cover z-[-5]"></div>
-    <div class="flex items-center justify-center h-[calc(100vh-125px)] ">
+    <div class="flex items-center justify-center  min-h-[calc(100vh-125px)] ">
 
-        <div class="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
+        <div class="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full shadow-gray-400 ">
             <h2 class="text-2xl font-bold text-center text-[#42BB8B] mb-6">Sign In</h2>
             <form>
                 <div class="mb-4">
@@ -21,17 +21,18 @@
                         class="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#42BB8B] focus:border-transparent"
                         required />
                 </div>
+
                 <button type="submit"
                     class="w-full bg-[#42BB8B] text-white font-bold py-2 rounded hover:bg-[#36a36b] transition duration-300">
                     Sign In
                 </button>
             </form>
             <p class="text-center text-gray-600 mt-4">
-                Don't have an account? <a href="#" class="text-[#42BB8B] hover:underline">Sign Up</a>
+                Don't have an account? <RouterLink to="/create-account" class="text-[#42BB8B] hover:underline">Create
+                    Account</RouterLink>
             </p>
         </div>
     </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
