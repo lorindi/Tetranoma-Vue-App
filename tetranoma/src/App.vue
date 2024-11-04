35ebf461902360b1ca3286686a7a62c2750eb769
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-04 10:26:11 +0200

    Update App.vue

--- tetranoma/src/App.vue
+++ tetranoma/src/App.vue
@@ -5,7 +5,7 @@ import Footer from '@/components/footer/Footer.vue'
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col items-center  ">
+    <div class="w-full h-full flex flex-col items-center  bg-[#FAFAFA]">
 
         <Navbar />
         <RouterView class="w-full h-full flex justify-center max-w-[1336px]" />
