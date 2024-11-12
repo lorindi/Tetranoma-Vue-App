5a3e59953c9ba0b04a6d12f82829f6a7fb1024f9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-12 21:37:49 +0200

    Update FiguresView.vue

--- tetranoma/src/views/FiguresView.vue
+++ tetranoma/src/views/FiguresView.vue
@@ -1,6 +1,11 @@
 <script setup>
+import CategoriesList from '@/components/homeSections/CategoriesList.vue';
+import Search from '@/components/search/Search.vue';
+
+
 
 </script>
 <template>
+    <Search size="default" />
     
 </template>
