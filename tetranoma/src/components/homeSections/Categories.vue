34bda139556e9fa246e60c1cd5a2cade2718a720
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-12 16:46:30 +0200

    Create Categories.vue

---
+++ tetranoma/src/components/homeSections/Categories.vue
@@ -0,0 +1,10 @@
+<script setup>
+
+import CategoriesList from './CategoriesList.vue';
+</script>
+
+<template>
+  <div class="xs:flex sm:flex md:flex">
+    <CategoriesList :limit="4" type="list" />
+  </div>
+</template>
