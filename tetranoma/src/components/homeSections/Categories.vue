00ad407b3f9d0909133d7b51035d65f0f604aece
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 13:40:26 +0200

    Update Categories.vue

--- tetranoma/src/components/homeSections/Categories.vue
+++ tetranoma/src/components/homeSections/Categories.vue
@@ -1,8 +1,19 @@
 <script setup>
 
+import { RouterLink } from 'vue-router';
 import CategoriesList from '../CategoriesList.vue';
 </script>
 
 <template>
-    <CategoriesList :limit="3" type="list" />
+    <div class="flex flex-col w-full max-w-[1336px] min-h-[350px] gap-14 ">
+        <div class="flex items-start justify-between px-[15px] lg:px-[30px]">
+            <h4 class=" font-semibold text-base lg:text-xl ">Most popular categories</h4>
+            <RouterLink to="/search-figure" class="flex gap-2 items-center">
+                <p class="font-medium text-base lg:text-lg">See all</p> <i class="pi pi-arrow-right text-[#8F3DBF] font-medium text-base lg:text-lg" />
+            </RouterLink>
+        </div>
+        <div class="">
+            <CategoriesList :limit="3" type="list" />
+        </div>
+    </div>
 </template>
