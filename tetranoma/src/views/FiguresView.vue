ab8fcea08cbbc47a8c992e2b668828349ef46515
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 21:29:05 +0200

    Update FiguresView.vue

--- tetranoma/src/views/FiguresView.vue
+++ tetranoma/src/views/FiguresView.vue
@@ -7,7 +7,7 @@ import Search from '@/components/search/Search.vue';
 
 </script>
 <template>
-    <div class="flex flex-col w-full max-w-[1336px] items-center justify-center">
+    <div class="flex flex-col w-full max-w-[1336px] items-center justify-center my-[50px]">
         <Search size="default" />
         <FiguresList />
     </div>
