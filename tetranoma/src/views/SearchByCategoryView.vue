908b387afcd3f597953aea5ac3aea304fa651d95
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-14 00:21:01 +0200

    Update SearchByCategoryView.vue

--- tetranoma/src/views/SearchByCategoryView.vue
+++ tetranoma/src/views/SearchByCategoryView.vue
@@ -6,6 +6,7 @@ import CategoriesList from '@/components/CategoriesList.vue';
 </script>
 <template>
     <div class="flex min-h-[calc(100vh-100px)] items-center justify-center w-full max-w-[1336px]">
-        <CategoriesList type="list" />
+        <CategoriesList type="list" :limit="11" />
+
     </div>
 </template>
