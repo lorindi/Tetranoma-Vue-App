9fca66de38d6e4424adc7ad11481c73fdd67222a
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-14 00:20:56 +0200

    Update Categories.vue

--- tetranoma/src/components/homeSections/Categories.vue
+++ tetranoma/src/components/homeSections/Categories.vue
@@ -7,10 +7,10 @@ import HeaderWithLink from '../HeaderWithLink.vue';
 
 <template>
     <div class="flex flex-col w-full max-w-[1336px] min-h-[350px] gap-14 my-[20px]">
-        <HeaderWithLink title="Most popular categories" link="/figures" linkText="See all" />
+        <HeaderWithLink title="Most popular categories" link="/search-figure" linkText="See all" />
 
         <div class="">
-            <CategoriesList :limit="4" type="list" />
+            <CategoriesList type="home" />
         </div>
     </div>
 </template>
