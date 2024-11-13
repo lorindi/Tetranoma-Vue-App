e7a5f7188177680e3a68a14883682e672e3c58ee
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 23:18:16 +0200

    Update Categories.vue

--- tetranoma/src/components/homeSections/Categories.vue
+++ tetranoma/src/components/homeSections/Categories.vue
@@ -2,16 +2,13 @@
 
 import { RouterLink } from 'vue-router';
 import CategoriesList from '../CategoriesList.vue';
+import HeaderWithLink from '../HeaderWithLink.vue';
 </script>
 
 <template>
     <div class="flex flex-col w-full max-w-[1336px] min-h-[350px] gap-14 my-[20px]">
-        <div class="flex items-start justify-between px-[15px] lg:px-[30px]">
-            <h4 class=" font-semibold text-base lg:text-xl ">Most popular categories</h4>
-            <RouterLink to="/search-figure" class="flex gap-2 items-center">
-                <p class="font-medium text-base lg:text-lg">See all</p> <i class="pi pi-arrow-right text-[#8F3DBF] font-medium text-base lg:text-lg" />
-            </RouterLink>
-        </div>
+        <HeaderWithLink title="Most popular categories" link="/figures" linkText="See all" />
+
         <div class="">
             <CategoriesList :limit="4" type="list" />
         </div>
