dc379c1608055040ad32b44ab34ae9de69e48fe9
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 21:29:00 +0200

    Update CardFigure.vue

--- tetranoma/src/components/cards/CardFigure.vue
+++ tetranoma/src/components/cards/CardFigure.vue
@@ -34,17 +34,19 @@ const isRatingVisible = computed(() => props.type === 'cardRating' && props.rati
 </script>
 
 <template>
-<div class="bg-white shadow-md rounded-lg overflow-hidden w-[100%] sm:w-[250px] md:w-[300px] lg:w-[350px] h-auto">
-    <RouterLink :to="`/figures/${link}`" class="flex flex-col h-full w-full">
-      <img :src="imgUrl" alt="Card image" class="w-full h-[150px] sm:h-[180px] md:h-[200px] lg:h-[220px] object-cover opacity-90" />
-      <div class="p-4">
-        <h2 class="text-lg sm:text-xl md:text-2xl font-semibold mb-2">{{ title.length > 17 ? title.slice(0, 17) + '...' : title }}</h2>
-        <p class="text-sm sm:text-base md:text-lg text-gray-700 mb-2">{{ description.length > 28 ? description.slice(0, 28) + '...' : description }}</p>
+    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[150px] my-[10px]
+    sm:w-[250px] sm:h-[300px] 
+    md:w-[300px] md:h-[350px] 
+    lg:w-[300px] lg:h-[360px] lg:my-[15px]
+    ">
+      <img :src="imgUrl" alt="Card image" class="w-full h-[50%] sm: md: lg: object-cover opacity-90" />
+      <div class="flex flex-col justify-evenly h-[50%] py-1 px-2 md:px-3 lg:px-5">
+        <h2 class="text-sm sm:text-xl md:text-2xl font-semibold mb-[3px]">{{ title.length > 17 ? title.slice(0, 17) + '...' : title }}</h2>
+        <p class="text-sm sm:text-base md:text-lg text-gray-700 mb-[3px]">{{ description.length > 28 ? description.slice(0, 28) + '...' : description }}</p>
         <div v-if="isRatingVisible" class="flex items-center">
           <span class="text-yellow-500 mr-1 text-sm sm:text-base md:text-lg">★</span>
           <span class="text-gray-700 text-sm sm:text-base md:text-lg">{{ rating }}</span>
         </div>
       </div>
     </RouterLink>
-  </div>
 </template>
