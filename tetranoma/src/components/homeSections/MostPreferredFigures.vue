3e32c5be8d53d82264da0bb1652e04e6ae0faa08
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 23:10:01 +0200

    Update MostPreferredFigures.vue

--- tetranoma/src/components/homeSections/MostPreferredFigures.vue
+++ tetranoma/src/components/homeSections/MostPreferredFigures.vue
@@ -1,9 +1,12 @@
 <script setup>
 import FiguresList from '../FiguresList.vue';
-
+import HeaderWithLink from '../HeaderWithLink.vue';
 
 </script>
 <template>
-    <FiguresList :limit="4" />
+    <div class="w-full max-w-[1336px]">
+        <HeaderWithLink title="Top Rated" link="/figures" linkText="Explore" />
+        <FiguresList :limit="4" />
+    </div>
 
 </template>
