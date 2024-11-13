9e1e10105ad355f79a2b76e000a9c98b7b6b3537
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-13 13:40:22 +0200

    Update CardCategory.vue

--- tetranoma/src/components/cards/CardCategory.vue
+++ tetranoma/src/components/cards/CardCategory.vue
@@ -27,8 +27,8 @@ defineProps({
     class="flex flex-col items-center justify-around py-[10px] px-[5px] mx-[3px] my-[10px] shadow-md 
     w-[90px] h-[90px]
     sm:w-[125px] sm:h-[125px] 
-    md:w-[125px] md:h-[125px] md:mx-[10px]
-    lg:flex-row lg:w-[280px] lg:h-[150px] lg:gap-[20px] lg:py-[10px] lg:px-[15px] lg:items-center lg:justify-center lg:shadow-none 
+    md:w-[150px] md:h-[125px] md:mx-[10px] md:flex-row
+    lg:flex-row lg:w-[280px] lg:h-[150px] lg:gap-[20px] lg:py-[10px] lg:px-[15px] lg:items-center lg:justify-center 
     ">
         <i :class="`pi ${icon} text-[#00BD7E] text-2xl bg-white p-0 rounded-md
         sm:text-3xl
