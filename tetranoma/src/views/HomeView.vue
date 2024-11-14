f4e146f2f9c3f9696e32ae8504ef69deb5c2475f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-14 12:53:49 +0200

    Update HomeView.vue

--- tetranoma/src/views/HomeView.vue
+++ tetranoma/src/views/HomeView.vue
@@ -2,11 +2,14 @@
 import Hero from '@/components/homeSections/Hero.vue';
 import Categories from '@/components/homeSections/Categories.vue';
 import MostPreferredFigures from '@/components/homeSections/MostPreferredFigures.vue';
+import Guide from '@/components/Guide.vue';
+import FigureJoySection from '@/components/homeSections/FigureJoySection.vue';
 
 </script>
 
 <template>
     <Hero />
     <Categories />
+    <FigureJoySection/>
     <MostPreferredFigures />
 </template>
