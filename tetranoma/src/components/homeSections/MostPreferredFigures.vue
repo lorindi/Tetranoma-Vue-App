068a08a40dc75ea8940bdf850c37244f8a33ef6c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-14 13:10:26 +0200

    Update MostPreferredFigures.vue

--- tetranoma/src/components/homeSections/MostPreferredFigures.vue
+++ tetranoma/src/components/homeSections/MostPreferredFigures.vue
@@ -4,7 +4,7 @@ import HeaderWithLink from '../HeaderWithLink.vue';
 
 </script>
 <template>
-    <div class="flex flex-col w-full max-w-[1336px] items-center">
+    <div class="flex flex-col w-full max-w-[1336px] lg:my-[70px] items-center">
         <HeaderWithLink title="Top Rated" link="/figures" linkText="Explore" />
         <FiguresList :limit="4" />
     </div>
