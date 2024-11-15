30c8ee96490c4f9e70c28241c80b1a3b656c4fa8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-15 10:53:22 +0200

    Update NavigationLinks.vue

--- tetranoma/src/components/NavigationLinks.vue
+++ tetranoma/src/components/NavigationLinks.vue
@@ -1,4 +1,3 @@
-<!-- NavigationLinks.vue -->
 <script setup>
 import { RouterLink, useRoute } from 'vue-router'
 
@@ -13,25 +12,31 @@ const isActiveLink = (routePath) => {
   const route = useRoute()
   return route.path === routePath
 }
+
+defineProps(['toggleMenu'])
 </script>
 
 <template>
   <RouterLink to="/"
+  @click="toggleMenu"
     :class="[isActiveLink('/') ? 'text-[#42BB8B]' : '', 'xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden  hover:text-[#117277] tracking-wide text-lg']">
     <i :class="['pi pi-home text-[#117277]']"></i>
   </RouterLink>
 
   <RouterLink to="/sign-in"
+  @click="toggleMenu"
     :class="[isActiveLink('/sign-in') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
     Sign In
   </RouterLink>
   
   <RouterLink to="/create-account"
+  @click="toggleMenu"
     :class="[isActiveLink('/create-account') ? 'text-[#42BB8B]' : '', 'hover:text-[#117277] tracking-wide text-lg']">
     Create Account
   </RouterLink>
 
   <RouterLink v-for="link in links" :key="link.path" :to="link.path"
+  @click="toggleMenu"
     :class="[isActiveLink(link.path) ? 'text-[#42BB8B]' : '', 'hover:text-[#117277]', 'tracking-wide text-lg']">
     {{ link.label }}
   </RouterLink>
