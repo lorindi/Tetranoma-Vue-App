91a78d1c6abde9d366cc0f3e063e8954212f8cd7
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-15 10:53:19 +0200

    Update Tablet.vue

--- tetranoma/src/components/navbar/Tablet.vue
+++ tetranoma/src/components/navbar/Tablet.vue
@@ -12,22 +12,29 @@ const isOpen = ref(false)
 const toggleMenu = () => {
     isOpen.value = !isOpen.value
 }
+
 </script>
 
 <template>
-    <div class="flex w-full h-full items-center justify-between px-[10px] relative">
-        <RouterLink to="/" class="">
+    <div class="flex w-full h-full items-center justify-between px-[10px] relative z">
+        <RouterLink to="/" class="z-10">
             <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
         </RouterLink>
 
         <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
 
-        <nav v-motion :initial="{ opacity: 0, translateY: -20 }" :enter="{ opacity: 1, translateY: 0 }"
+        <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-[4]" @click="toggleMenu"></div>
+
+        <nav v-motion 
+            :initial="{ opacity: 0, translateY: -20 }" 
+            :enter="{ opacity: 1, translateY: 0 }"
             :leave="{ opacity: 0, translateY: -20 }"
-            :transition="{ enter: { duration: 0.5 }, leave: { duration: 1.0 } }" v-if="isOpen"
-            class="absolute flex flex-col w-[100%] items-center justify-center top-[125px] left-0 bg bg-[#fafafa] h-[250px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
+            :transition="{ enter: { duration: 0.5 }, 
+            leave: { duration: 1.0 } }" 
+            v-if="isOpen"
+            class="absolute flex flex-col w-[100%] items-center justify-center top-[0px] left-0 bg bg-[#fafafa] h-[550px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
             <div class="flex gap-10">
-                <NavigationLinks />
+                <NavigationLinks :toggleMenu="toggleMenu" />
             </div>
             <Search size="default" />
         </nav>
