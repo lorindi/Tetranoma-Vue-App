1a7823235aa0601c41f398b37fee0367fc2266fc
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-15 10:53:16 +0200

    Update Mobile.vue

--- tetranoma/src/components/navbar/Mobile.vue
+++ tetranoma/src/components/navbar/Mobile.vue
@@ -15,21 +15,22 @@ const toggleMenu = () => {
 
 <template>
     <div class="flex w-full h-full items-center justify-between px-[10px] relative">
-        <RouterLink to="/" class="">
+        <RouterLink to="/" class="z-10">
             <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
         </RouterLink>
 
         <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
+        <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-[4]" @click="toggleMenu"></div>
 
         <nav v-motion :initial="{ opacity: 0, translateX: -20 }" :enter="{ opacity: 1, translateX: 0 }"
             :leave="{ opacity: 0, translateX: -20 }"
             :transition="{ enter: { duration: 0.5 }, leave: { duration: 1.0 } }" v-if="isOpen"
-            class="absolute flex flex-col w-[100%] items-center justify-center top-[125px] left-0 bg bg-[#fafafa] h-auto gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
+            class="absolute flex flex-col w-[100%] items-center justify-center top-[0px] left-0 bg bg-[#fafafa] h-screen gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg shadow-gray-400">
 
             <Search size="default" />
           
 
-            <NavigationLinks />
+            <NavigationLinks :toggleMenu="toggleMenu" />
         </nav>
     </div>
 
