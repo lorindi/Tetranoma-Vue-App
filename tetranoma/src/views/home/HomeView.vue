cd05cc2600d489434967e2a5bc1a78e988ab275d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 00:24:15 +0200

    Create HomeView.vue

---
+++ tetranoma/src/views/home/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Hero from '@/views/home/sections/Hero.vue';
+import Categories from '@/views/home/sections/Categories.vue';
+import MostPreferredFigures from '@/views/home/sections/MostPreferredFigures.vue';
+// import Guide from '@/components/Guide.vue';
+import FigureJoySection from '@/views/home/sections/FigureJoySection.vue';
+
+console.log("HomeView component mounted")
+</script>
+
+<template>
+    <div class="w-full h-full flex flex-col items-center">
+        <Hero />
+        <Categories />
+        <FigureJoySection/>
+        <MostPreferredFigures />
+    </div>
+</template>
