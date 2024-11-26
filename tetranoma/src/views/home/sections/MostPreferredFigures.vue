ecd1c0e885e11f2b8b87bda95627948d2c4c7c07
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 00:24:24 +0200

    Create MostPreferredFigures.vue

---
+++ tetranoma/src/views/home/sections/MostPreferredFigures.vue
@@ -0,0 +1,12 @@
+<script setup>
+import FiguresList from '@/components/lists/FiguresList.vue';
+import HeaderWithLink from '@/components/HeaderWithLink.vue';
+
+</script>
+<template>
+    <div class="flex flex-col w-full max-w-[1336px] lg:my-[70px] items-center">
+        <HeaderWithLink title="Top Rated" link="/figures" linkText="Explore" />
+        <FiguresList :limit="4" />
+    </div>
+
+</template>
