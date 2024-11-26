c7621249f455749ac2a48c4b7d04d85057f1ca83
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 00:24:17 +0200

    Create Categories.vue

---
+++ tetranoma/src/views/home/sections/Categories.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import { RouterLink } from 'vue-router';
+import CategoriesList from '@/components/lists/CategoriesList.vue';
+import HeaderWithLink from '@/components/HeaderWithLink.vue';
+</script>
+
+<template>
+    <div class="flex flex-col w-full max-w-[1336px] min-h-[350px] gap-14 my-[20px]">
+        <HeaderWithLink title="Most popular categories" link="/search-figure" linkText="See all" />
+
+        <div class="">
+            <CategoriesList type="home" />
+        </div>
+    </div>
+</template>
