8fe44720f1aed2b23a0eb158cade53da9a5d4d52
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 01:17:29 +0200

    Update App.vue

--- tetranoma/src/App.vue
+++ tetranoma/src/App.vue
@@ -2,10 +2,12 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/navbar/Navbar.vue'
 import Footer from '@/components/footer/Footer.vue'
+import DarkMode from './components/darkMode/DarkMode.vue';
 </script>
 
 <template>
     <div class="w-full h-full flex flex-col items-center  bg-[#FAFAFA]">
+        <DarkMode />
         <Navbar />
         <RouterView class="w-full h-full flex justify-center max-w-[1336px]" />
         <Footer />
