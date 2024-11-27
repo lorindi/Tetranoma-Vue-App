e309211f8202ddce960e65d6b71862d7649ab29f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 11:20:45 +0200

    Update SignInView.vue

--- tetranoma/src/views/SignInView.vue
+++ tetranoma/src/views/SignInView.vue
@@ -15,28 +15,28 @@ const handleSubmit = (e) => {
 <template>
     <div class="flex items-center justify-center  min-h-[calc(100vh-125px)] ">
 
-        <div class="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full shadow-gray-400 ">
+        <div class="bg-white shadow-xl rounded-lg p-8 max-w-sm w-full shadow-gray-400 dark:shadow-gray-900 dark:bg-gray-800">
             <h2 class="text-2xl font-bold text-center text-[#42BB8B] mb-6">Sign In</h2>
             <form @submit="handleSubmit">
                 <div class="mb-4">
-                    <label for="email" class="block text-gray-700 font-medium mb-2">Email</label>
+                    <label for="email" class="block text-gray-700 font-medium mb-2 dark:text-white">Email</label>
                     <input 
                         v-model="email"
                         type="email" 
                         id="email" 
                         placeholder="Enter your email"
-                        class="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#42BB8B] focus:border-transparent"
+                        class="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#42BB8B] focus:border-transparent dark:bg-gray-700 dark:text-white"
                         required 
                     />
                 </div>
                 <div class="mb-6">
-                    <label for="password" class="block text-gray-700 font-medium mb-2">Password</label>
+                    <label for="password" class="block text-gray-700 font-medium mb-2 dark:text-white">Password</label>
                     <input 
                         v-model="password"
                         type="password" 
                         id="password" 
                         placeholder="Enter your password"
-                        class="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#42BB8B] focus:border-transparent"
+                        class="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-[#42BB8B] focus:border-transparent dark:bg-gray-700 dark:text-white"
                         required 
                     />
                 </div>
