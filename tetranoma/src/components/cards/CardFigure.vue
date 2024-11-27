eb7ea594e1a998810331c160d4427398e4ac4e4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-11-27 09:58:07 +0200

    Update CardFigure.vue

--- tetranoma/src/components/cards/CardFigure.vue
+++ tetranoma/src/components/cards/CardFigure.vue
@@ -34,7 +34,7 @@ const isRatingVisible = computed(() => props.type === 'cardRating' && props.rati
 </script>
 
 <template>
-    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[150px] my-[10px]
+    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[150px] my-[10px] dark:bg-gray-800
     sm:w-[250px] sm:h-[300px] 
     md:w-[300px] md:h-[350px] 
     lg:w-[300px] lg:h-[360px] lg:my-[15px]
@@ -42,10 +42,10 @@ const isRatingVisible = computed(() => props.type === 'cardRating' && props.rati
       <img :src="imgUrl" alt="Card image" class="w-full h-[50%] sm: md: lg: object-cover opacity-90" />
       <div class="flex flex-col justify-evenly h-[50%] py-1 px-2 md:px-3 lg:px-5">
         <h2 class="text-sm sm:text-xl md:text-2xl font-semibold mb-[3px]">{{ title.length > 17 ? title.slice(0, 17) + '...' : title }}</h2>
-        <p class="text-sm sm:text-base md:text-lg text-gray-700 mb-[3px]">{{ description.length > 28 ? description.slice(0, 28) + '...' : description }}</p>
+        <p class="text-sm sm:text-base md:text-lg text-gray-700 mb-[3px] dark:text-gray-400">{{ description.length > 28 ? description.slice(0, 28) + '...' : description }}</p>
         <div v-if="isRatingVisible" class="flex items-center">
-          <span class="text-yellow-500 mr-1 text-sm sm:text-base md:text-lg">★</span>
-          <span class="text-gray-700 text-sm sm:text-base md:text-lg">{{ rating }}</span>
+          <span class="text-yellow-500 mr-1 text-sm sm:text-base md:text-lg ">★</span>
+          <span class="text-gray-700 text-sm sm:text-base md:text-lg dark:text-white">{{ rating }}</span>
         </div>
       </div>
     </RouterLink>
