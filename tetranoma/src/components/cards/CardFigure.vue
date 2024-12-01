18430225e8b0c6f9292c99232b6b4661f648a2b3
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-01 20:05:13 +0200

    Update CardFigure.vue

--- tetranoma/src/components/cards/CardFigure.vue
+++ tetranoma/src/components/cards/CardFigure.vue
@@ -34,13 +34,13 @@ const isRatingVisible = computed(() => props.type === 'cardRating' && props.rati
 </script>
 
 <template>
-    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[140px] my-[10px] dark:bg-gray-700 z-10
+    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[140px] my-[10px] dark:bg-gray-900 dark:shadow-gray-900 z-10
     sm:w-[250px] sm:h-[300px] 
     md:w-[300px] md:h-[350px] 
     lg:w-[300px] lg:h-[360px] lg:my-[15px]
     ">
-      <img :src="imgUrl" alt="Card image" class="w-full h-[50%] sm: md: lg: object-cover opacity-90" />
-      <div class="flex flex-col justify-evenly h-[50%] py-1 px-2 md:px-3 lg:px-5">
+      <img :src="imgUrl" alt="Card image" class="w-full h-[60%] rounded-lg sm: md: lg: object-cover opacity-90" />
+      <div class="flex flex-col justify-center h-[40%] py-1 px-2 md:px-3 lg:px-5">
         <h2 class="text-sm sm:text-xl md:text-2xl font-semibold mb-[3px]">{{ title.length > 17 ? title.slice(0, 17) + '...' : title }}</h2>
         <p class="text-sm sm:text-base md:text-lg text-gray-700 mb-[3px] dark:text-gray-400">{{ description.length > 28 ? description.slice(0, 28) + '...' : description }}</p>
         <div v-if="isRatingVisible" class="flex items-center">
