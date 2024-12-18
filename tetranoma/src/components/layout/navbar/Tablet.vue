895b626cf62246935d4e7f31b0f11ff301fcf2d4
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-18 12:54:04 +0200

    Create Tablet.vue

---
+++ tetranoma/src/components/layout/navbar/Tablet.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import Search from '@/components/ui/search/Search.vue'
+import logo from '@/assets/logo.jpg'
+import NavigationLinks from '@/components/common/NavigationLinks.vue'
+import ToggleButton from './ToggleButton.vue';
+
+
+const isOpen = ref(false)
+
+const toggleMenu = () => {
+    isOpen.value = !isOpen.value
+}
+
+</script>
+
+<template>
+    <div class="flex w-full h-full items-center justify-between px-[10px] relative z">
+        <RouterLink to="/" class="z-10">
+            <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
+        </RouterLink>
+
+        <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
+
+        <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-[4]" @click="toggleMenu"></div>
+
+        <nav v-motion 
+            :initial="{ opacity: 0, translateY: -20 }" 
+            :enter="{ opacity: 1, translateY: 0 }"
+            :leave="{ opacity: 0, translateY: -20 }"
+            :transition="{ enter: { duration: 0.5 }, 
+            leave: { duration: 1.0 } }" 
+            v-if="isOpen"
+            class="absolute flex flex-col w-[100%] pt-[100px] items-center justify-center top-[0px] left-0 bg bg-[#fafafa] h-[550px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg dark:bg-gray-900">
+            <div class="flex gap-10">
+                <NavigationLinks :toggleMenu="toggleMenu" />
+            </div>
+            <Search size="default" />
+        </nav>
+    </div>
+
+</template>
