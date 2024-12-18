2ac4e38a99f15e28972c30876b6dddc4ad4b6196
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-18 12:53:59 +0200

    Create Mobile.vue

---
+++ tetranoma/src/components/layout/navbar/Mobile.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import Search from '@/components/ui/search/Search.vue'
+import logo from '@/assets/logo.jpg'
+import NavigationLinks from '@/components/common/NavigationLinks.vue'
+import ToggleButton from './ToggleButton.vue';
+
+const isOpen = ref(false)
+
+const toggleMenu = () => {
+    isOpen.value = !isOpen.value
+}
+</script>
+
+<template>
+    <div class="flex w-full h-full items-center justify-between px-[10px] relative">
+        <RouterLink to="/" class="z-30">
+            <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
+        </RouterLink>
+
+        <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
+        <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-[4]" @click="toggleMenu"></div>
+
+        <nav v-motion :initial="{ opacity: 0, translateX: -20 }" :enter="{ opacity: 1, translateX: 0 }"
+            :leave="{ opacity: 0, translateX: -20 }"
+            :transition="{ enter: { duration: 0.5 }, leave: { duration: 1.0 } }" v-if="isOpen"
+            class="absolute flex flex-col w-[100%] items-center justify-center pt-[100px] top-[0px] left-0 bg bg-[#fafafa] h-screen gap-[50px] py-[30px] z-20 rounded-br-lg rounded-bl-lg shadow-lg dark:bg-gray-900">
+
+            <Search size="default" />
+          
+
+            <NavigationLinks :toggleMenu="toggleMenu" />
+        </nav>
+    </div>
+
+</template>
