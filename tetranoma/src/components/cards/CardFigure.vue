1c4ba3ce3cb6a98e8773fa9a65a2368b3c8432c6
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2024-12-28 15:04:49 +0200

    Update CardFigure.vue

--- tetranoma/src/components/cards/CardFigure.vue
+++ tetranoma/src/components/cards/CardFigure.vue
@@ -34,7 +34,7 @@ const isRatingVisible = computed(() => props.type === 'cardRating' && props.rati
 </script>
 
 <template>
-    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[140px] my-[10px] dark:bg-gray-900 dark:shadow-gray-900 z-10
+    <RouterLink :to="`/figures/${link}`" class="flex flex-col bg-white shadow-md rounded-lg overflow-hidden h-[200px] w-[140px] my-[10px] dark:bg-gray-900 dark:shadow-gray-900 z-5
     sm:w-[250px] sm:h-[300px] 
     md:w-[300px] md:h-[350px] 
     lg:w-[300px] lg:h-[360px] lg:my-[15px]
