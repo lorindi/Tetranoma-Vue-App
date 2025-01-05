d73bd65e8d929c0281cbd535bc825bd4a6cdc51d
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-05 19:14:46 +0200

    Update MostPreferredFigures.vue

--- tetranoma/src/views/home/sections/MostPreferredFigures.vue
+++ tetranoma/src/views/home/sections/MostPreferredFigures.vue
@@ -1,12 +1,52 @@
 <script setup>
-import FiguresList from '@/components/lists/FiguresList.vue';
-import HeaderWithLink from '@/components/HeaderWithLink.vue';
+import FiguresList from "@/components/lists/FiguresList.vue"
+import HeaderWithLink from "@/components/HeaderWithLink.vue"
+import { useFiguresStore } from "@/stores/useFiguresStore"
+import { onMounted } from "vue"
+import { useRouter } from "vue-router"
 
+const router = useRouter()
+const figuresStore = useFiguresStore()
+
+const handleExplore = () => {
+    console.log("Navigating to figures with top rated sort")
+    router.push({
+        path: "/figures",
+        query: {
+            sortBy: "rating",
+            sortOrder: "desc"
+        }
+    })
+}
+
+onMounted(async () => {
+    console.log("Fetching top rated figures for home page")
+    await figuresStore.getFigures(1, {
+        sortBy: "rating",
+        sortOrder: "desc",
+        limit: 4
+    })
+})
 </script>
 <template>
     <div class="flex flex-col w-full max-w-[1336px] lg:my-[70px] items-center">
-        <HeaderWithLink title="Top Rated" link="/figures" linkText="Explore" />
-        <FiguresList :limit="4" />
+        <HeaderWithLink 
+            title="Top Rated" 
+            linkText="Explore" 
+            @click="handleExplore"
+        />
+        <div v-if="figuresStore.loading" class="text-center py-4">
+            Loading...
+        </div>
+        <FiguresList 
+            v-else
+            :figures="figuresStore.figures"
+            :limit="4"
+            sortBy="rating"
+            sortOrder="desc"
+        />
     </div>
+
+
 
 </template>
