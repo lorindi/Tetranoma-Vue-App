351a8a6baf52ef356729bc7f259c85264f784f88
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 00:11:00 +0200

    Update Navbar.vue

--- tetranoma/src/components/layout/navbar/Navbar.vue
+++ tetranoma/src/components/layout/navbar/Navbar.vue
@@ -5,13 +5,13 @@ import Web from './Web.vue'
 </script>
 
 <template>
-    <div class="hidden w-full h-full sm:flex-col xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900">
+    <div class="hidden w-full h-full sm:flex-col xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Mobile />
     </div>
-    <div class="hidden w-full h-full md:flex sm:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900">
+    <div class="hidden w-full h-full md:flex sm:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Tablet />
     </div>
-    <div class="hidden w-full h-full items-center justify-center lg:flex xl:flex 2xl:flex md:hidden sm:hidden bg-[#FAFAFA] dark:bg-gray-900">
+    <div class="hidden w-full h-full items-center justify-center lg:flex xl:flex 2xl:flex md:hidden sm:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Web />
     </div>
 </template>
