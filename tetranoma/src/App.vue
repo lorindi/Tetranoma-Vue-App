59bedba7bf5d4231af1ed9db731913b677b4aebb
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-06 15:39:31 +0200

    Update App.vue

--- tetranoma/src/App.vue
+++ tetranoma/src/App.vue
@@ -2,7 +2,7 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from '@/components/layout/navbar/Navbar.vue'
 import Footer from '@/components/layout/footer/Footer.vue'
-import DarkMode from './components/ui/darkMode/DarkMode.vue';
+import DarkMode from './components/ui/DarkMode.vue';
 // import MouseGradient from './components/MouseGradient.vue';
 </script>
 
