bd56f839c52e2f117f7204184c5a5789a4619e4f
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 16:06:41 +0200

    Update NaviIcons.vue

--- tetranoma/src/components/layout/navbar/NaviIcons.vue
+++ tetranoma/src/components/layout/navbar/NaviIcons.vue
@@ -9,11 +9,11 @@ console.log("NavIcons component initialized")
 </script>
 
 <template>
-    <div class="flex items-center gap-[10px] md:gap-[30px] text-lg">
-        <RouterLink to="/profile" class="hover:text-[#117277] tracking-wide text-lg">
+    <div class="flex items-center gap-[10px] md:gap-[30px] text-xl">
+        <RouterLink to="/profile" class="hover:text-[#117277] tracking-wide ">
             <i class="pi pi-heart mr-2"></i>
         </RouterLink>
-        <RouterLink to="/figures" class="hover:text-[#117277] tracking-wide text-lg">
+        <RouterLink to="/figures" class="hover:text-[#117277] tracking-wide ">
             <i class="pi pi-shopping-bag mr-2"></i>
         </RouterLink>
         <CartSidebar ref="cartSidebarRef" />
