830122ecc1290ef13a95e95e59cead54cb58c6e8
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 15:05:22 +0200

    Update Tablet.vue

--- tetranoma/src/components/layout/navbar/Tablet.vue
+++ tetranoma/src/components/layout/navbar/Tablet.vue
@@ -38,26 +38,33 @@ onUnmounted(() => {
             <img :src="logo" alt="" class="rounded-full p-[10px] w-[125px] " />
         </RouterLink>
 
-        <div class="flex items-center gap-4 z-20">
+        <div class="flex items-center gap-[35px] text-lg z-15">
+            <RouterLink to="/favorites" class="hover:text-[#117277] tracking-wide text-lg">
+                <i class="pi pi-heart mr-2"></i>
+            </RouterLink>
+            <RouterLink to="/figures" class="hover:text-[#117277] tracking-wide text-lg">
+                <i class="pi pi-shopping-bag mr-2"></i>
+            </RouterLink>
             <CartSidebar ref="cartSidebarRef" />
+        </div>
+
+        <div class="flex items-center gap-4 z-20">
             <ToggleButton :isOpen="isOpen" @toggle="toggleMenu" />
         </div>
 
         <div v-if="isOpen" class="fixed inset-0 bg-black bg-opacity-50 z-15" @click="toggleMenu"></div>
 
-        <nav v-motion 
-            :initial="{ opacity: 0, translateY: -20 }" 
-            :enter="{ opacity: 1, translateY: 0 }"
-            :leave="{ opacity: 0, translateY: -20 }"
-            :transition="{ enter: { duration: 0.5 }, 
-            leave: { duration: 1.0 } }" 
-            v-if="isOpen"
+        <nav v-motion :initial="{ opacity: 0, translateY: -20 }" :enter="{ opacity: 1, translateY: 0 }"
+            :leave="{ opacity: 0, translateY: -20 }" :transition="{
+                enter: { duration: 0.5 },
+                leave: { duration: 1.0 }
+            }" v-if="isOpen"
             class="absolute flex flex-col w-[100%] pt-[100px] items-center justify-center top-[0px] left-0 bg bg-[#fafafa] h-[550px] gap-[50px] py-[30px] z-[5] rounded-br-lg rounded-bl-lg shadow-lg dark:bg-gray-900">
             <div class="flex gap-10">
                 <NavigationLinks :toggleMenu="toggleMenu" />
             </div>
             <Search size="default" />
-            
+
         </nav>
     </div>
 
