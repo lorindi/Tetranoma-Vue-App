6b6be81615d6e26e9d48d07680d624746c6ce5e5
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 18:24:10 +0200

    Create Title.vue

---
+++ tetranoma/src/components/common/Title.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useMotion } from "@vueuse/motion"
+import { computed, ref } from "vue"
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+
+})
+
+
+const titleRef = ref(null)
+
+const titleMotion = useMotion(titleRef, {
+  initial: { opacity: 0, y: -50 },
+  enter: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 800, type: "spring" }
+  }
+})
+
+</script>
+
+<template>
+  <h1 ref="titleRef" class="font-semibold text-2xl sm:text-3xl md:text-4xl text-[#00BD7E] mb-1 md:mb-8 text-center">{{ title }}
+  </h1>
+</template>
