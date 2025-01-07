323241d04ba3e0950df4ff38137505aab9c12eda
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-07 15:33:34 +0200

    Create NaviIcons.vue

---
+++ tetranoma/src/components/layout/navbar/NaviIcons.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue"
+import { RouterLink } from "vue-router"
+import CartSidebar from "@/components/CartSidebar.vue"
+
+const cartSidebarRef = ref(null)
+
+console.log("NavIcons component initialized")
+</script>
+
+<template>
+    <div class="flex items-center gap-[10px] md:gap-[30px] text-lg">
+        <RouterLink to="/favorites" class="hover:text-[#117277] tracking-wide text-lg">
+            <i class="pi pi-heart mr-2"></i>
+        </RouterLink>
+        <RouterLink to="/figures" class="hover:text-[#117277] tracking-wide text-lg">
+            <i class="pi pi-shopping-bag mr-2"></i>
+        </RouterLink>
+        <CartSidebar ref="cartSidebarRef" />
+    </div>
+</template>
