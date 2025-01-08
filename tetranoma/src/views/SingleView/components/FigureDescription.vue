7698cf2b18cbc27b48f75cf2a920cfb30f7e2dc0
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-08 15:40:40 +0200

    Create FigureDescription.vue

---
+++ tetranoma/src/views/SingleView/components/FigureDescription.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  description: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="p-6 border-t border-gray-200 dark:border-gray-700">
+    <h2 class="text-xl font-semibold text-gray-800 dark:text-white mb-4">Description</h2>
+    <p class="text-gray-600 dark:text-gray-400 leading-relaxed">
+      {{ description }}
+    </p>
+  </div>
+</template>
