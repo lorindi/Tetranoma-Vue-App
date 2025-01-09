21caaa2452fe2e8d22126f7dd08dfbe144895a89
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-09 23:48:46 +0200

    Update main.js

--- tetranoma/src/main.js
+++ tetranoma/src/main.js
@@ -7,11 +7,16 @@ import 'primeicons/primeicons.css';
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 import { createPinia } from 'pinia';
-
+import PrimeVue from 'primevue/config';
 const app = createApp(App)
 const pinia = createPinia();
 
-
+app.use(PrimeVue);
+// app.use(PrimeVue, {
+//     theme: {
+//         preset: Aura
+//     }
+// });
 app.use(router)
 app.use(MotionPlugin);
 app.use(Toast);
