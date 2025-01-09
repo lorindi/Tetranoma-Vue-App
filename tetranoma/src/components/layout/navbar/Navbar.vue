d0874466ed638a7b7002a714432c4abae5173236
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-09 20:41:52 +0200

    Update Navbar.vue

--- tetranoma/src/components/layout/navbar/Navbar.vue
+++ tetranoma/src/components/layout/navbar/Navbar.vue
@@ -2,16 +2,33 @@
 import Mobile from './Mobile.vue'
 import Tablet from './Tablet.vue'
 import Web from './Web.vue'
+
+import { useRoute } from "vue-router"
+import { computed } from "vue"
+
+const route = useRoute()
+
+const excludedPaths = [
+    "/admin"
+]
+
+const showNavbar = computed(() => {
+    const isExcluded = excludedPaths.some(path => route.path.startsWith(path))
+    return !isExcluded
+})
 </script>
 
 <template>
-    <div class="hidden w-full h-full sm:flex-col xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
+    <div v-if="showNavbar"
+        class="hidden w-full h-full sm:flex-col xs:flex sm:flex md:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Mobile />
     </div>
-    <div class="hidden w-full h-full md:flex sm:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
+    <div v-if="showNavbar"
+        class="hidden w-full h-full md:flex sm:hidden lg:hidden xl:hidden 2xl:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Tablet />
     </div>
-    <div class="hidden w-full h-full items-center justify-center lg:flex xl:flex 2xl:flex md:hidden sm:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
+    <div v-if="showNavbar"
+        class="hidden w-full h-full items-center justify-center lg:flex xl:flex 2xl:flex md:hidden sm:hidden bg-[#FAFAFA] dark:bg-gray-900 z-50">
         <Web />
     </div>
 </template>
