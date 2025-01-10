3e6941be55167125561ceb7ea17b5a81dc5c2854
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-01-10 12:33:49 +0200

    Update axios.js

--- tetranoma/src/api/axios.js
+++ tetranoma/src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: "https://tetranoma-express-api.vercel.app/api",
+
   withCredentials: true,
   headers: {
     "Content-Type": "application/json"
