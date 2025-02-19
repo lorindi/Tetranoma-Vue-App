41c73b125b8a87f747c1f757b9e2117c4b127a7c
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-02-19 12:42:55 +0200

    Update App.vue

--- tetranoma/src/App.vue
+++ tetranoma/src/App.vue
@@ -7,11 +7,12 @@ import DarkMode from './components/ui/DarkMode.vue';
 </script>
 
 <template>
-    <div class="w-full h-full flex flex-col items-center bg-[#FAFAFA] dark:bg-gray-900 dark:text-white">
+    <div class="w-full h-full flex flex-col items-center text-gray-900 bg-[#FAFAFA] dark:bg-gray-900 dark:text-white">
         <!-- <MouseGradient />    -->
         <DarkMode />
         <Navbar />
-        <RouterView class="w-full h-full my-15 flex justify-start max-w-[1336px]" />
+        <RouterView class="w-full h-full my-15 flex justify-start max-w-[1336px]
+        " />
         <Footer />
     </div>
 </template>
