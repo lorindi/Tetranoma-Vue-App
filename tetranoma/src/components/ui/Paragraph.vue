395827c375465636485849a8df9fa80195c16105
Lora Mitova <92224899+lorindi@users.noreply.github.com>
2025-03-10 14:34:17 +0200

    Create Paragraph.vue
    Created a reusable Paragraph component with different color options and responsive design

---
+++ tetranoma/src/components/ui/Paragraph.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { computed } from "vue";
+const props = defineProps({
+
+  color: {
+    type: String,
+    default: "default",
+    validator: (value) => ["primary", "transparent"].includes(value)
+  }
+});
+
+const colorText = computed(() => {
+  switch (props.color) {
+    case "primary":
+      return "text-gray-800 dark:text-gray-300";
+    case "transparent":
+      return "text-gray-700 dark:text-gray-400";
+    default:
+      return "text-gray-700 dark:text-gray-300";
+  }
+})
+
+
+</script>
+
+<template>
+<p class="text-base lg:text-lg " :class="[colorText]"><slot></slot></p>
+</template>
